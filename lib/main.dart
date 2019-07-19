0b42917c9c1ea433faa134fc8b05812b89875334
wilson <whuanca06@gmail.com>
2019-07-19 15:32:32 -0400

    adding email auth an google auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,232 +1,134 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
-void main() => runApp(MyApp());
+
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Auth-Firebase',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'Auth - Firabase',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _imageUrl;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Board-auth"),
-          centerTitle: true,
-          backgroundColor: Colors.lightGreen,
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Board"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
+              new Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: FlatButton(
-                  child: Text("Google-Sign In"),
-                  onPressed: () => _gSignIn(),
-                  color: Colors.green,
+                  child: Text("Google-Sign-in"),
+                  onPressed: () => _gSignin(),
+                  color: Colors.red,
                 ),
               ),
-              Padding(
+              new Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: FlatButton(
-                  child: Text("Sig In with Email"),
-                  color: Colors.blueAccent,
-                  onPressed: () {},
+                  color: Colors.orange,
+                  child: Text("Signin with Email"),
+                  onPressed: () => _signInWithEmail(),
                 ),
               ),
-              Padding(
+              new Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: FlatButton(
+                  color: Colors.purple,
                   child: Text("Create Account"),
-                  color: Colors.purpleAccent,
-                  onPressed: () {},
+                  onPressed: () => _createUser(),
                 ),
-              )
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  color: Colors.redAccent,
+                  child: Text("Logout"),
+                  onPressed: () => _logout(),
+                ),
+              ),
+              new Image.network(_imageUrl == null || _imageUrl.isEmpty
+                  ? "https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true"
+                  : _imageUrl)
             ],
           ),
         ));
   }
 
-  Future<FirebaseUser>_gSignIn() async {
+  Future<FirebaseUser> _gSignin() async {
     GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    GoogleSignInAuthentication googleSignInAuthentication =
+    await googleSignInAccount.authentication;
 
-    FirebaseUser user = await _auth.signInWithGoogle(idToken: googleSignInAuthentication.idToken, accessToken: googleSignInAuthentication.accessToken);
-    print("user is :${user.displayName}");
+    FirebaseUser user = await _auth.signInWithGoogle(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    print("User is: ${user.photoUrl}");
+
+    setState(() {
+      _imageUrl = user.photoUrl;
+    });
+
+    return user;
+  }
+
+  Future _createUser() async {
+    FirebaseUser user = await _auth
+        .createUserWithEmailAndPassword(
+        email: "ramthegreaterer@gmail.com", password: "hiltongrace2345")
+        .then((userNew) {
+      print("User created ${userNew.displayName}");
+      print("Email: ${userNew.email}");
+    });
+
+    //print(user.email);
+  }
+
+  _logout() {
+    setState(() {
+      _googleSignIn.signOut();
+      _imageUrl = null;
+    });
+  }
+
+  _signInWithEmail() {
+
+    _auth
+        .signInWithEmailAndPassword(
+        email: "ramthegreaterer@gmail.com", password: "hiltongrace2345")
+        .catchError((error) {
+      print("Something went wrong! ${error.toString()}");
+    }).then((newUser) {
+      print("User signed in: ${newUser.email}");
+    });
   }
 }
-
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: ThemeData(
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: MyHomePage(),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  List<Board> boardMessages = List();
-//  Board board;
-//  final FirebaseDatabase database = FirebaseDatabase.instance;
-//  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-//  DatabaseReference databaseReference;
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//    board = Board("", "");
-//    databaseReference = database.reference().child("community_board");
-//    databaseReference.onChildAdded.listen(_onEntryAdded);
-//    databaseReference.onChildAdded.listen(_onEntryChanged);
-//  }
-//
-//  //  void _incrementCounter() {
-////    //setting a map to firebase whit the method set
-////    database
-////        .reference()
-////        .child("message")
-////        .set({"firstname": "wilson", "lastname": "huanca", "Age": "24"});
-////    setState(() {
-////      database
-////          .reference()
-////          .child("message")
-////          .once()
-////          .then((DataSnapshot snapshot) {
-////        Map<dynamic, dynamic> data = snapshot.value;
-////
-////        print("values from firebase DB ${data.values}");
-////      });
-////      _counter++;
-////    });
-////  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        appBar: AppBar(
-//          title: Text("Board"),
-//        ),
-//        body: Column(
-//          children: <Widget>[
-//            Flexible(
-//              flex: 0,
-//              child: Center(
-//                child: Form(
-//                  key: formkey,
-//                  child: Flex(
-//                    direction: Axis.vertical,
-//                    children: <Widget>[
-//                      ListTile(
-//                        leading: Icon(Icons.subject),
-//                        title: TextFormField(
-//                          initialValue: "",
-//                          onSaved: (val) => board.subjet = val,
-//                          validator: (val) => val == "" ? val : null,
-//                        ),
-//                      ),
-//                      ListTile(
-//                        leading: Icon(Icons.message),
-//                        title: TextFormField(
-//                          initialValue: "",
-//                          onSaved: (val) => board.body = val,
-//                          validator: (val) => val == "" ? val : null,
-//                        ),
-//                      ),
-//                      //send or post button
-//                      FlatButton(
-//                        child: Text("post"),
-//                        color: Colors.redAccent,
-//                        onPressed: () {
-//                          handleSubmit();
-//                        },
-//                      )
-//                    ],
-//                  ),
-//                ),
-//              ),
-//            ),
-//            Flexible(
-//              //getting the data from firebase
-//              child: FirebaseAnimatedList(
-//                query: databaseReference,
-//                itemBuilder: (_, DataSnapshot snapshot,
-//                    Animation<double> animation, int index) {
-//                  return new Card(
-//
-//                    child: ListTile(
-//                      leading: CircleAvatar(
-//                        backgroundColor: Colors.redAccent,
-//                      ),
-//                      title: Text(boardMessages[index].subjet),
-//                      subtitle: Text(boardMessages[index].body),
-//                    ),
-//                  );
-//                },
-//              ),
-//            )
-//          ],
-//        ));
-//  }
-//
-//  void _onEntryAdded(Event event) {
-//    setState(() {
-//      boardMessages.add(Board.fromSnapshot(event.snapshot));
-//    });
-//  }
-//
-//  void handleSubmit() {
-//    final FormState form = formkey.currentState;
-//    if (form.validate()) {
-//      form.save();
-//      form.reset();
-//      // save form data to database
-//      // create a unique key
-//      databaseReference.push().set(board.toJson());
-//    }
-//  }
-//
-//  void _onEntryChanged(Event event) {
-//    var oldEntry = boardMessages.singleWhere((entry){
-//      return entry.key == event.snapshot.key;
-//    });
-//    setState(() {
-//      boardMessages[boardMessages.indexOf(oldEntry)] = Board.fromSnapshot(event.snapshot);
-//    });
-//  }
-//}
